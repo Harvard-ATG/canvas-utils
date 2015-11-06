7bbaaab5bbe80e4f14b5018e85cbe14afc1745fa
Arthur Barrett <abarrett@fas.harvard.edu>
2015-11-06 15:04:02 -0500

    Update get_users_emails.py

--- skeleton/get_users_emails.py
+++ skeleton/get_users_emails.py
@@ -4,22 +4,24 @@ from canvas_sdk.utils import get_all_list_data
 from canvas_sdk import RequestContext
 import sys
 
-# Get the course ID from the CLI arguments
+# Get the course ID from the command line
 course_id = sys.argv[1]
 if course_id is None:
-    course_id = "dummy"
+    sys.exit("Error: missing course_id")
 
-# Setup the request context with a large pagination limit to limit the total number of request
-# needed for larger courses
-request_context = RequestContext(OAUTH_TOKEN, CANVAS_URL, per_page=1000)
+# Setup the request context with a large pagination limit (minimize # of requests)
+request_context = RequestContext(OAUTH_TOKEN, CANVAS_URL, per_page=100)
 
-# Note: need to use get_all_list_data() to follow the paged results that
-# the canvas API returns.
+# NOTE: you must use get_all_list_data() in order to follow the paginated results
+# and get all the data.
+#
+# If you just call the method directly, you'll get a single page (max 100 results)
+# which may or may not include everyone if there are >100 students in the course.
 results = get_all_list_data(request_context, courses.list_users_in_course_users, course_id, "email", enrollment_type="student")
 
 # Extract and sort the results we want. 
 users = sorted([(x['email'], x['name']) for x in results], key=lambda x: x[0])
 
-# Print the names ane emails in CSV foramt
+# Print the names and emails in CSV foramt
 for idx, user in enumerate(users):
     print "%s,%s" % user
