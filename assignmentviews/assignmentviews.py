b2834d4504dc341d55697111ba99a456f3ff75c1
JaZahn <jcleveng@fas.harvard.edu>
2015-11-12 15:43:43 -0500

    using get_user_emails as a base for assignmentviews

---
+++ assignmentviews/assignmentviews.py
@@ -0,0 +1,29 @@
+from settings.secure import OAUTH_TOKEN, CANVAS_URL
+from canvas_sdk.methods import courses
+from canvas_sdk.utils import get_all_list_data
+from canvas_sdk import RequestContext
+import sys
+
+# Get the course ID from the command line
+course_id = None
+if len(sys.argv) == 2:
+    course_id = sys.argv[1]
+else:
+    sys.exit("Error: missing course_id")
+
+# Setup the request context with a large pagination limit (minimize # of requests)
+request_context = RequestContext(OAUTH_TOKEN, CANVAS_URL, per_page=100)
+
+# NOTE: you must use get_all_list_data() in order to follow the paginated results
+# and get all the data.
+#
+# If you just call the method directly, you'll get a single page (max 100 results)
+# which may or may not include everyone if there are >100 students in the course.
+results = get_all_list_data(request_context, courses.list_users_in_course_users, course_id, "email", enrollment_type="student")
+
+# Extract and sort the results we want. 
+users = sorted([(x['email'], x['name']) for x in results], key=lambda x: x[0])
+
+# Print the names and emails in CSV foramt
+for idx, user in enumerate(users):
+    print "%s,%s" % user
